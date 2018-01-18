ec42135c561f9b0315bc75a6cde24d12936c07d4
Simon Rogers <webdev@centralblue.co.uk>
2018-01-18 12:48:56 +0000

    Update model

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -2,6 +2,8 @@
 
 namespace Midnite81\Salesforce\Model;
 
+use App\Services\Auth;
+use GuzzleHttp\Client;
 use Midnite81\Salesforce\Exceptions\ConnectionNotSetException;
 
 abstract class Model
@@ -34,6 +36,41 @@ abstract class Model
      */
     abstract protected function fillableFields();
 
+    public function __construct($attributes = [])
+    {
+        $this->fillAttributes($attributes);
+    }
+
+    public function find($id)
+    {
+        $describeUrl = $this->getConnection() . '/' . $id;
+
+        try {
+            $client = new \App\Services\Client();
+            $response = $client->request($describeUrl, null, Auth::authorisationHeader());
+        } catch (\Exception $e) {
+            return 'Could not retrieve data: ' . $e->getMessage() . $e->getTraceAsString();
+
+        }
+
+        return json_decode($response->getBody()->getContents());
+    }
+
+    /**
+     * Fill Attributes
+     *
+     * @param $data
+     */
+    protected function fillAttributes(array $data = [])
+    {
+        if (! empty($data)) {
+            $this->attributes = array_merge($this->fillableFields(), $data);
+        } else {
+            $this->attributes = $this->fillableFields();
+        }
+
+    }
+
 
     /**
      * Get Connection String
@@ -51,6 +88,16 @@ abstract class Model
     }
 
     /**
+     * Get Object Name
+     *
+     * @return string
+     */
+    public function getObjectName()
+    {
+        return basename($this->objectUrl());
+    }
+
+    /**
      * Get primary key
      *
      * @return bool|null
@@ -58,6 +105,23 @@ abstract class Model
     public function getId()
     {
         return (! empty($this->attributes['id'])) ?? null;
+    }
+
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
+    public function save()
+    {
+        $client = new \App\Services\Client();
+        try {
+            $response = $client->request($this->objectUrl(), $this->attributes, Auth::authorisationHeader());
+        } catch (\Exception $e) {
+            dd($e->getMessage(), $e->getTraceAsString());
+        }
+
+        dd($response->getBody()->getContents());
     }
 
     /**
@@ -68,6 +132,26 @@ abstract class Model
     public function __toString()
     {
         return $this->getId();
+    }
+
+    /**
+     * Get the definition of the object
+     *
+     * @return string
+     */
+    public function describe()
+    {
+        $describeUrl = config('salesforce.instance') . '/services/data/v20.0/sobjects/' . $this->getObjectName() . '/describe';
+
+        try {
+            $client = new \App\Services\Client();
+            $response = $client->request($describeUrl, null, Auth::authorisationHeader());
+        } catch (\Exception $e) {
+            return 'Could not retrieve data: ' . $e->getMessage() . $e->getTraceAsString();
+
+        }
+
+        return $response->getBody()->getContents();
     }
 
 
